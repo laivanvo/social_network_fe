3d847e9a882cf0d13f298d50886c00d9b182a2d1
laivanvo <laivanvo2000s@gmail.com>
2022-05-19 22:29:28 +0700

    19-5-night

--- src/views/pages/group/ListGroup.vue
+++ src/views/pages/group/ListGroup.vue
@@ -1,31 +1,48 @@
 <template>
-  <div class="row">
-      <create-group/>
-    <div v-for="item in groups" :key="item.id">
-        {{item.name}}
+    <div class="row">
+        <create-group />
+        <div v-for="item in groups" :key="item.id">
+            <div class="card" style="width: 18rem">
+                <img
+                    :src="'http://localhost:8080' + item.avatar"
+                    class="card-img-top"
+                />
+                <div class="row card-body">
+                    <p class="row card-text">
+                        welcome to {{ item.name }}
+                    </p>
+                    <p class="row card-text">
+                        Author:  {{ item.user.profile.last_name + " " + item.user.profile.first_name}}
+                    </p>
+                    <button class="row btn btn-primary" @click="joinGroup()">Join group</button>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import BaseRequest from "@/helpers/BaseRequest";
-import CreateGroup from '@/views/pages/group/CreateGroup.vue'
+import CreateGroup from "@/views/pages/group/CreateGroup.vue";
 export default {
     components: {
         CreateGroup,
     },
-  data() {
-    return {
-      groups: [],
-    };
-  },
-  methods: {
-    getGroup() {
-      let _this = this;
-      BaseRequest.get("groups").then((res) => {
-        _this.groups = res.data.groups.data;
-      });
+    data() {
+        return {
+            groups: [],
+        };
     },
-  },
+    mounted() {
+        this.getGroup();
+    },
+    methods: {
+        getGroup() {
+            let _this = this;
+            BaseRequest.get("groups").then((res) => {
+                _this.groups = res.data.groups;
+            });
+        },
+    },
 };
 </script>
